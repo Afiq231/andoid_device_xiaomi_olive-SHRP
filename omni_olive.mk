29a149056516e4001b991e84c5609f78f99bef1a
Afiq231 <56428569+Afiq231@users.noreply.github.com>
2020-02-29 16:40:58 +0800

    Update omni_olive.mk

--- omni_olive.mk
+++ omni_olive.mk
@@ -19,10 +19,10 @@ PRODUCT_RELEASE_NAME := olive
 
 $(call inherit-product, build/target/product/embedded.mk)
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
-$(call inherit-product, vendor/shrp/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_PACKAGES += \
     charger_res_images \
